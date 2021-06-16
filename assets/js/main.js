9a32f7a567eeec4e9141ed519e56d0949b20404a
Oleksandr Sainidi <saynidi96@gmail.com>
2021-06-16 14:17:10 +0300

    Добавление проекта

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function(){
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500)
+	});
+});
+
+// $(document).ready(function () {
+
+//   $('.header-navbar__hamburger').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').toggleClass('active');
+//   });
+
+// });
