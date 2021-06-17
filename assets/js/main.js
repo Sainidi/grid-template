064357993251ab4adf34d889c4c4fd4b746f8d31
Oleksandr Sainidi <saynidi96@gmail.com>
2021-06-17 19:20:26 +0300

    Создал все секции на странице

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,3 @@ $(document).ready(function(){
     $('.header-navbar__list').stop(true, true).slideToggle(500)
 	});
 });
-
-// $(document).ready(function () {
-
-//   $('.header-navbar__hamburger').on('click', function () {
-//     $(this).toggleClass('active');
-//     $('.header-navbar__list').toggleClass('active');
-//   });
-
-// });
